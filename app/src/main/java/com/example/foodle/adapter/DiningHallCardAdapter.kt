bad2c4b057e3c9d979d4da83a1a7c216f9ad570d
Craig <craig.leeson@utexas.edu>
2022-11-27 04:39:06 -0600

    tweaked RV cells and redirect icon image color

--- app/src/main/java/com/example/foodle/adapter/DiningHallCardAdapter.kt
+++ app/src/main/java/com/example/foodle/adapter/DiningHallCardAdapter.kt
@@ -11,16 +11,9 @@ import androidx.recyclerview.widget.ListAdapter
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.foodle.FoodDishActivity
 import com.example.foodle.R
-import androidx.activity.viewModels
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.DiffUtil
-import com.example.foodle.data.DataSource
 import com.example.foodle.model.FoodData
-import com.example.foodle.overview.OverviewViewModel
-import com.google.android.material.card.MaterialCardView
 
 /**
  * Adapter to inflate the appropriate list item layout and populate the view with information
@@ -43,9 +36,8 @@ class DiningHallCardAdapter(
         // Declare and initialize the fruit name
         val dishName: TextView = view!!.findViewById(R.id.food_name)
         // Declare and initialize the fruit color
-        val dishAllergens: TextView = view!!.findViewById(R.id.food_allergen)
-//        // Declare and initialize the fruit's common dishes
-//        val fruitDishes: TextView = view!!.findViewById(R.id.fruit_dishes)
+        val dishAllergens: TextView = view!!.findViewById(R.id.food_category)
+        val redirect: TextView = view!!.findViewById(R.id.nutrition_redirect_text)
         val card = view?.findViewById<CardView>(R.id.dining_hall_card)
     }
 
@@ -84,14 +76,13 @@ class DiningHallCardAdapter(
             context?.startActivity(webIntent)
         }
 
-        // Set the image resource for the current fruit
-//        holder.fruitImage.setImageResource(item.imageResourceId)
-        // Set the text for the current dish's name
+
         holder.dishName.text = item.name
-        // Set the text for the current fruit's color
         holder.dishAllergens.text = item.category
-        // Set the text for the current fruit's common dishes
-//        resources?.getString(R.string.fruit_taste, item.dishes).also { holder.fruitDishes.text = it }
+
+        if (item.name == "Food Not Served") {
+            holder.redirect.visibility = View.INVISIBLE
+        }
     }
 
     private fun test() {
